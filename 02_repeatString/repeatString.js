db270a8bf1dd0528f94ba1ca150477b080262abc
trangza <86011739+trangza@users.noreply.github.com>
2025-04-24 11:11:06 +0700

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeatCount) {
+  let repeatedWord = "";
+  for (i = 0; i < repeatCount; i++) {
+    repeatedWord += word;
+  }
+  if (repeatCount < 0) {
+    repeatedWord = "ERROR";
+  }
+  return repeatedWord;
 };
 
 // Do not edit below this line
